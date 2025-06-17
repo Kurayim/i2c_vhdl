962c29a9f9857d5627e3780da11db85d17e72ca8
Mohsen Jahed <161829820+Kurayim@users.noreply.github.com>
2025-06-17 06:37:50 -0700

    Update crc_8.v

--- crc_8.v
+++ crc_8.v
@@ -1,57 +1,94 @@
-`default_nettype none
+`timescale 1ns / 1ps
+//////////////////////////////////////////////////////////////////////////////////
+// Company: 
+// Engineer: 
+// 
+// Create Date: 06/15/2025 11:41:45 PM
+// Design Name: 
+// Module Name: crc_8
+// Project Name: 
+// Target Devices: 
+// Tool Versions: 
+// Description: 
+// 
+// Dependencies: 
+// 
+// Revision:
+// Revision 0.01 - File Created
+// Additional Comments:
+// 
+//////////////////////////////////////////////////////////////////////////////////
 
-/*
- * crc8 -- Calculates the crc8 value for a stream of bytes using a static polynomial.
- *
- * clk_i -- Input clock
- * rst_i -- Resets the internal crc value. Set high to start calculating the crc8 value for a new stream of data. This
- *          signal is synchronus to the clock.
- * data_i -- The input data stream
- * data_valid_i -- Indicates that the data_i signal is valid, and that the CRC value should be recalculated
- * crc_o -- The current computed CRC-8 value for the data stream. It is updated one clock cycle after data_valid_i goes
- *          high.
- */
-module crc8 #(
-    parameter POLYNOMIAL=8'h07
-) (
-    input wire clk_i,
-    input wire rst_i,
-    input wire [7:0] data_i,
-    input wire data_valid_i,
-    output wire [7:0] crc_o
-);
 
-wire [7:0] crc_xor_data, table_out;
-reg [7:0] crc_save;
-reg data_valid_last_cycle;
-initial {data_valid_last_cycle, crc_save} = 0;
+module crc_8(
+    input wire clk,
+    input wire rst,
+    input wire run,
+    input wire[7:0] data_in,
+    output reg ready,
+    output[7:0] crc
+    );
+    
+    
+    
+    parameter IDLE = 2'b00, XOR_ASS = 2'b01, CAL_CRC = 2'b10;
+    parameter BITS = 8;
+    parameter POLY = 8'h31;
+    
+    
+    reg[7:0] val = 8'hff;
+    reg[7:0] num = 0;
+    reg[1:0] state   = IDLE;
+    reg old_run      = 0;
+    
 
-always @(posedge clk_i)
-    if (rst_i)
-        data_valid_last_cycle <= 0;
-    else
-        data_valid_last_cycle <= data_valid_i;
-
-crc_table #(
-    .POLYNOMIAL(POLYNOMIAL)
-) crc_table (
-    .clk_i(clk_i),
-    .addr_i(crc_xor_data),
-    .value_o(table_out)
-);
-
-assign crc_xor_data = crc_o ^ data_i;
-always @(posedge clk_i)
-    if (rst_i)
-        crc_save <= 0;
-    else
-        if (data_valid_last_cycle)
-            crc_save <= table_out;
-        else
-            crc_save <= crc_save;
-
-assign crc_o = data_valid_last_cycle? table_out : crc_save;
-
+    
+    
+    always@(posedge clk)begin
+        
+        old_run <= run;
+        if(rst == 0)begin
+            state <= IDLE;
+        end
+        else begin
+            case(state)
+                IDLE:begin
+                    val   <= 8'hff;
+                    num   <= 8'h00;
+                    state <= XOR_ASS;
+                end
+                XOR_ASS:begin
+                    
+                    if(old_run == 0 & run == 1)begin
+                        val   <= val ^ data_in;
+                        state <= CAL_CRC;
+                        
+                    end
+                end
+                CAL_CRC:begin
+                    num <= num + 1;
+                    if(num <= 7)begin
+                        val <= {val[BITS-2:0], 1'b0} ^ (val[BITS-1] ? POLY : 0);
+                    end
+                    else begin
+                        state <= XOR_ASS;
+                        ready <= 1;
+                    end
+                end
+                default: state <= IDLE;
+            endcase
+        end
+    end
+    
+    
+    assign crc = val;
+    
+    
 endmodule
 
-`default_nettype wire
+
+
+
+
+
+
