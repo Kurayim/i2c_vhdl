52b64db31212be41624faddbb64668dd0461dc90
Mohsen Jahed <161829820+Kurayim@users.noreply.github.com>
2025-07-05 06:34:55 -0700

    Update seven_segment.vhd

--- seven_segment.vhd
+++ seven_segment.vhd
@@ -36,12 +36,12 @@ entity seven_segment is
     generic(clk_freq    : integer :=  50000000; 
             num_segment : integer :=  5
          );
-    Port ( clk       : in STD_LOGIC;
-           reset     : in STD_LOGIC;
-           bit_seg   : in STD_LOGIC_VECTOR  (((Num_segment*5) - 1)  downto 0);
-           bit_point : in STD_LOGIC_VECTOR  ((Num_segment - 1)      downto 0);
-           seg_en    : out STD_LOGIC_VECTOR ((num_segment - 1)      downto 0);
-           seg_led   : out STD_LOGIC_VECTOR (7 downto 0)
+    Port ( clk       : in  std_logic;
+           reset     : in  std_logic;
+           bit_seg   : in  std_logic_vector  (((Num_segment*4) - 1)  downto 0);
+           bit_point : in  std_logic_vector  ((Num_segment - 1)      downto 0);
+           seg_en    : out std_logic_vector ((num_segment - 1)      downto 0);
+           seg_led   : out std_logic_vector (7 downto 0)
          );
 end seven_segment;
 
@@ -52,9 +52,82 @@ architecture Behavioral of seven_segment is
     signal counter_clk      :   unsigned(15 downto 0);
     signal num_seg          :   unsigned((num_segment - 1) downto 0);
     
+    signal segmant_en       :   std_logic_vector((num_segment - 1) downto 0);
+    signal segmant_bit      :   unsigned(((num_segment*4) - 1) downto 0);
+    signal points           :   unsigned((num_segment - 1) downto 0);
+    
+    signal flag_next        : std_logic;
+    signal old_flag_next    : std_logic;
+    
     
     
 begin
+    
+    
+    seg_en <= segmant_en;
+    
+    enable_seg : process(clk)
+    begin
+        if(rising_edge(clk))then
+            
+            old_flag_next <= flag_next;
+            
+            if(reset = '0')then
+                segmant_en <= std_logic_vector(to_unsigned(1,num_segment));
+            elsif(old_flag_next = '0'  and  flag_next = '1')then
+                segmant_en <= std_logic_vector(unsigned(segmant_en)  ror  1);
+            end if;
+            
+        end if;
+    end process;
+    
+    select_segment : process(clk)
+    begin
+        if(rising_edge(clk))then
+            
+            if(reset = '0')then
+                segmant_en <= std_logic_vector(to_unsigned(1,num_segment));
+            elsif(old_flag_next = '0'  and  flag_next = '1')then
+                if(num_seg = to_unsigned(0,num_segment))then
+                    segmant_bit <= unsigned(bit_seg);
+                    points      <= unsigned(bit_point);
+                else
+                    segmant_bit <= unsigned(segmant_bit) ror 4;
+                    points <= unsigned(points) ror 1;
+                end if;
+            end if;
+                
+                seg_led(0) <= points(1);
+                case(segmant_bit(3 downto 0)) is
+                    when "0000" =>
+                        seg_led(6 downto 0) <= "1000000";
+                    when "0001" =>
+                        seg_led(6 downto 0) <= "1111001";
+                    when "0010" =>
+                        seg_led(6 downto 0) <= "0100100";
+                    when "0011" =>
+                        seg_led(6 downto 0) <= "0110000";
+                    when "0100" =>
+                        seg_led(6 downto 0) <= "0011001";
+                    when "0101" =>
+                        seg_led(6 downto 0) <= "0010010";
+                    when "0110" =>
+                        seg_led(6 downto 0) <= "0000011";
+                    when "0111" =>
+                        seg_led(6 downto 0) <= "1111000";
+                    when "1000" =>
+                        seg_led(6 downto 0) <= "0000000";
+                    when "1001" =>
+                        seg_led(6 downto 0) <= "0011000";
+                    when others =>
+                        seg_led(6 downto 0) <= "1111111";
+                end case;
+            
+        end if;
+    end process;   
+    
+    
+    
     
     
     select_seg : process(clk)
@@ -262,16 +335,24 @@ begin
             counter_clk <= counter_clk + 1;
             
             
-            if(reset = '1')then
+            if(counter_clk > to_unsigned(7,3))then
+                flag_next   <= '0';
+            end if;
+            
+            if(reset = '0')then
+                flag_next <= '0';
                 num_seg <= (others => '0');
                 counter_clk <= (others => '0');
             elsif(counter_clk >= NUM_COUNTER)then
                 counter_clk <= (others => '0');
-                num_seg <= num_seg + 1;
-                if(num_seg >= num_segment)then
+                flag_next   <= '1';
+                num_seg     <= num_seg + 1;
+                if(num_seg >= num_segment - 1)then
                     num_seg <= (others => '0');
                 end if;
+                
             end if;
+            
         end if;
     end process;
     
